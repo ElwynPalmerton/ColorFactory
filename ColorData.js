0bbb48f44f5cdba45f2330eecd9c528100b0d2e3
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2021-11-09 18:36:48 -0800

    Add color defaults

--- ColorData.js
+++
@@ -1,8 +0,0 @@
-const swatch = {
-    h: 150,
-    s: 70,
-    l: 70,
-    a: 1
-};
-
-export {swatch}
--- sketch.js
+++ sketch.js
@@ -1,16 +1,12 @@
 import {test, hello} from './Test.js';
-import CircleGroup from './CircleGroup.js';
+import CircleGroup from './circles/CircleGroup.js';
 
 let circle;
 let circles = [];
 
 window.setup = function() {
-  hello();
-  console.log('test hello: ', test);
-  
   colorMode(HSL);
   createCanvas(640, 600);
-  background(50);
 
   circles = new CircleGroup(1, 1);
 }
@@ -18,8 +14,10 @@ window.setup = function() {
 
 
 window.draw = function() {
+
   background(50);
   circles.update();
   circles.show();
+
 };
  
